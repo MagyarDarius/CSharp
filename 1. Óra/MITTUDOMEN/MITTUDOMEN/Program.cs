75f60495b5926795c54220ce20c07f7accd197cd
Cockroachcrusader <magyar.darius2008@gmail.com>
2025-09-08 08:43:10 +0200

    .

---
+++ 1. Óra/MITTUDOMEN/MITTUDOMEN/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MITTUDOMEN
+{
+    internal class Program
+    {
+        struct termek
+        {
+            public string Termek;
+            public int Ar;
+
+            public termek(string termek, int ar)
+            {
+                Termek = termek;
+                Ar = ar;
+            }
+
+            public override string ToString()
+            {
+                return $"{Termek} , {Ar} Ft";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<termek> products = ProcessCsvData();
+
+            foreach (var product in products)
+            {
+                Console.WriteLine(product);
+            }
+        }
+
+        static List<termek> ProcessCsvData()
+        {
+            List<termek> products = new List<termek>();
+
+            try
+            {
+                string[] lines = File.ReadAllLines("termekek.csv");
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string[] values = lines[i].Split(';');
+
+                    if (values.Length >= 2)
+                    {
+                        string termek = values[0];
+                        if (int.TryParse(values[1], out int ar))
+                        {
+                            products.Add(new termek(termek, ar));
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Hiba az ár értelmezésében: {values[1]}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Hiba a fájl beolvasásakor: {ex.Message}");
+            }
+
+            return products;
+        }
+    }
+}
---
+++ 1. Óra/MITTUDOMEN/MITTUDOMEN/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MITTUDOMEN")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MITTUDOMEN")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4d6d3339-ff86-49e5-bac6-68e3834a474b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
