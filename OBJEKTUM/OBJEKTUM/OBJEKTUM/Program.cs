8db455929f202d4ce97a3b3e8521d9c620661234
Cockroachcrusader <magyar.darius2008@gmail.com>
2025-09-26 08:42:52 +0200

    .

---
+++ OBJEKTUM/OBJEKTUM/OBJEKTUM/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBJEKTUM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dog bodri = new dog();
+            bodri.name = "Bodri";
+            bodri.age = 3;
+            dog buksi = new dog();
+            buksi.name = "Buksi";
+            buksi.age = 5;
+        }
+    } 
+    internal class dog
+    {
+        public string name;
+        public int age;
+    }
+}
