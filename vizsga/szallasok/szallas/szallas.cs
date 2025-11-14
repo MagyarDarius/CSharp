141b891e02b3fb50587dfb917b6f40e028749978
Cockroachcrusader <magyar.darius2008@gmail.com>
2025-11-14 08:41:13 +0100

    .

---
+++ vizsga/szallasok/szallas/szallas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szallas
+{
+    public class szallas
+    {
+        public string nev{ get; private set; }
+        public string cím { get; private set; }
+        public int szobaszam { get; private set; }
+        public int agyszam { get; private set; }
+        public string uzemeltető { get; private set; }
+        public string uzemeltetőcím { get; private set; }
+        public string uzemeltetőtelefon { get; private set; }
+        public bool status { get; private set; }
+        public string tipus { get; private set; }
+    }
+}
