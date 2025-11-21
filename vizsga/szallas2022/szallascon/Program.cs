6f1ac50bb0f2969928805f49a900b1f141bc2054
Cockroachcrusader <magyar.darius2008@gmail.com>
2025-11-21 08:44:48 +0100

    .

--- vizsga/szallas2022/szallascon/Program.cs
+++ vizsga/szallas2022/szallascon/Program.cs
@@ -11,7 +11,7 @@ namespace szallascon
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {   
             var lista = szallas.FileBetoltes("szallas2022.csv");
             // 4.feladat
             feladat(4);
@@ -20,7 +20,27 @@ namespace szallascon
             //5.feladat
             feladat(5);
             Console.WriteLine($"Átlagos ágyszám az aktív szálláshelyeken {(double)szallas.aktívAgyszam/(double)szallas.aktívSzallasokSzama:f2}");
-
+            //6.feladat
+            feladat(6);
+            Console.WriteLine("Szeghalom környéki aktív szállások");
+            Dictionary<string, int> telepulesDict = new Dictionary<string, int>();
+            foreach(var szallas in lista)
+            {
+                string telepulesNeve=szallas.SzallasCime.Split(' ')[1];
+                if(!szallas.Statusz) continue;
+                if (telepulesDict.ContainsKey(telepulesNeve))
+                {
+                    telepulesDict[telepulesNeve]++;
+                }
+                else
+                {
+                    telepulesDict.Add(telepulesNeve, 1);
+                }
+            }
+            foreach(var sor in telepulesDict)
+            {
+                Console.WriteLine($"\t{sor.Key} : {sor.Value} db");
+            }
             Console.ReadKey();
         }
         public static void feladat(int sorszam)
