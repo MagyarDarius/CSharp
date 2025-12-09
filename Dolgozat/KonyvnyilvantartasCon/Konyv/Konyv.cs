c72c25271467d7c8253298c2c531335f6e4ed469
Cockroachcrusader <magyar.darius2008@gmail.com>
2025-12-09 08:40:46 +0100

    .

---
+++ Dolgozat/KonyvnyilvantartasCon/Konyv/Konyv.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonyvDLL
+{
+    public class Konyv
+    {
+        public string Cim { get; private set; }
+        public string Szerzo { get; private set; }
+        public int LapokSzama { get; private set; }
+
+        public int KonyvekSzama = 0;
+
+        public Konyv(string cim, string szerzo, int lapokSzama)
+        {
+            this.Cim = cim;
+            this.Szerzo = szerzo;
+            this.LapokSzama = lapokSzama;
+
+            KonyvekSzama++;
+        }
+
+        public int OlvasasiIdo()
+        {
+            return LapokSzama * 2;
+        }
+    }
+}
---
+++ Dolgozat/KonyvnyilvantartasCon/Konyvnyilvantartas/Konyvnyilvantartas.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KonyvDLL;
+
+namespace Konyvnyilvantartas
+{
+    internal class Konyvnyilvantartas
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<Konyv> konyvek = new List<Konyv>();
+
+                while (true)
+                {
+                    Console.Write("Könyv címe (stop = kilépés): ");
+                    string cim = Console.ReadLine();
+
+                    if (cim == "stop")
+                        break;
+
+                    Console.Write("Kérek egy Szerzőt: ");
+                    string szerzo = Console.ReadLine();
+
+                    Console.Write("Kérem Az Oldalszámot: ");
+                    int lapok = int.Parse(Console.ReadLine());
+
+                    Konyv K = new Konyv(cim, szerzo, lapok);
+
+                    konyvek.Add(K);
+
+
+                    Console.WriteLine("Könyv rögzítve.\n");
+                }
+                foreach (Konyv k in konyvek)
+                {
+                    Console.WriteLine($"Cím: {k.Cim},\n Szerző: {k.Szerzo},\n Oldalszám: {k.LapokSzama},\n Olvasási idő: {k.OlvasasiIdo()} perc\n");
+                }
+                Console.WriteLine($"Az összes könyvek száma{konyvek.Count}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hiba a kódban" + ex.Message);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
