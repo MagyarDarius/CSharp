4728cdca944360fd61a990f5d72620bba567faba
Cockroachcrusader <magyar.darius2008@gmail.com>
2025-12-12 08:08:30 +0100

    .

---
+++ Pontszámok/DolgozatPontszamokCon/DolgozatPontszamok/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using TanulokDolgozat;
+
+namespace DolgozatPontszamok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Add meg a dolgozat maximális pontszámát: ");
+            int MaxPont = int.Parse(Console.ReadLine());
+            Tanulok.BeallitMaxPont(MaxPont);
+
+            List<Tanulok> diakok = new List<Tanulok>();
+
+            while (true)
+            {
+                Console.Write("\nDiák neve (vége = kilépés): ");
+                string nev = Console.ReadLine();
+
+                if (nev.ToLower() == "vége" || nev.ToLower() == "vege")
+                    break;
+
+                Console.Write("Elért pontszám: ");
+                int pont = int.Parse(Console.ReadLine());
+
+                diakok.Add(new Tanulok(nev, pont));
+            }
+
+            Console.WriteLine("\n--- EREDMÉNYEK ---\n");
+
+            foreach (var d in diakok)
+            {
+                Console.WriteLine($"Név: {d.Nev}");
+                Console.WriteLine($"Pontszám: {d.Pontszam}/{Tanulok.MaxPontszam}");
+                Console.WriteLine($"Százalék: {d.Szazalek():0.00}%");
+                Console.WriteLine($"Jegy: {d.Jegy()}");
+            }
+
+        }
+    }
+}
---
+++ Pontszámok/DolgozatPontszamokCon/Tanulok/Tanulok.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TanulokDolgozat
+{
+    public class Tanulok
+    {
+        public string Nev { get; private set; }
+        public int Pontszam { get; private set; }
+        public static int MaxPontszam { get; private set; }
+
+        public Tanulok(string nev, int pontszam)
+        {
+            this.Nev = nev;
+            this.Pontszam = pontszam;
+        }
+
+        public static void BeallitMaxPont(int maxPont)
+        {
+            MaxPontszam = maxPont;
+        }
+
+        public double Szazalek()
+        {
+            return (double)Pontszam / MaxPontszam * 100.0;
+        }
+
+        public int Jegy()
+        {
+            double sz = Szazalek();
+
+            if (sz >= 90) return 5;
+            if (sz >= 75) return 4;
+            if (sz >= 60) return 3;
+            if (sz >= 40) return 2;
+            return 1;
+        }
+    }
+}
